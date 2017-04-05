e3ffa14fe670c18bd09dfcfa39203431d8a26fa8
Lionel Andrieu <lionel.andrieu@ensea.fr>
2017-04-04 23:12:55 -0400

    Update preferences.js

--- routes/preferences.js
+++ routes/preferences.js
@@ -2,7 +2,7 @@ var express = require('express');
 
 var router = express.Router();
 var mongojs = require('mongojs');
-var db = mongojs('mongodb://landrieu:toulouse3123@ds139470.mlab.com:39470/mytasks-test');
+var db = mongojs('mongodb://xxx:xxxx3123@ds139470.mlab.com:39470/mytasks-test');
 
 
 
